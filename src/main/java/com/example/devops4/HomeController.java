f1b7eb105df10821ed3307872fb9450a34812add
ceylin <fatmaceylin2004@gmail.com>
2026-06-03 09:33:28 +0300

    webhook triggerlama testi 5

--- src/main/java/com/example/devops4/HomeController.java
+++ src/main/java/com/example/devops4/HomeController.java
@@ -8,7 +8,7 @@ public class HomeController {
 
 	@GetMapping("/")
 	public String index() {
-		return "Welcome to DevOps4! Webhook test ediyoz";
+		return "Welcome to DevOps4! Webhook test ";
 	}
 
 }
